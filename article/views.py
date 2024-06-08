81d5040b36f7e35d00dd6cee6038834ef3749fbd
nahiba33 <nahibeesgerova@gmail.com>
2024-06-08 17:52:17 +0400

    İlk commit mesajı

--- article/views.py
+++ article/views.py
@@ -1,20 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
+from .forms import ArticleForm
+from django.contrib import messages
 
 # Create your views here.
 def home__view(request):
     return render(request, "index.html")
 
-def our__articles(request):
+def articles(request):
     articles = Article.objects.all()
-    return render(request, "our__articles.html", {'articles' : articles})
+    return render(request, "articles.html", {'articles' : articles})
 
-def my__articles(request):
-    articles = Article.objects.all()
-    return render(request, "my__articles.html", {'articles' : articles})
+def add_article(request):
+    form = ArticleForm(request.POST or None, request.FILES or None)
+
+    if form.is_valid():
+        article = form.save(commit=False)
+        article.author = request.user
+        article.save()
+        # save() -> hemise commiti True olaraq qebul edir.
+        messages.success(request, "Article is added.")
+        return redirect("dashboard")
+    context = {"form" : form}
+    return render(request, "add_article.html", context)
+
+def dashboard(request):
+    articles = Article.objects.filter(author = request.user)
+    return render(request, "dashboard.html", {'articles' : articles})
 
 def about__view(request):
     return render(request, "about.html")
 
 def contact__view(request):
     return render(request, "contact.html")
+
+def article_detail(request, id):
+    article = Article.objects.filter(id=id).first()
+    context = {"article" : article}
+    return render(request, "article_detail.html", context)
--- core/urls.py
+++ core/urls.py
@@ -5,8 +5,10 @@ from article.views import *
 urlpatterns = [
     path("", home__view, name="home"),
     path("about/", about__view, name="about"),
-    path("our__articles", our__articles, name="our__articles"),
-    path("my__articles", my__articles, name="my__articles"),
+    path("articles", articles, name="articles"),
+    path("dashboard", dashboard, name="dashboard"),
+    path("add_article", add_article, name="add_article"),
+    path("article_detail/<int:id>", article_detail, name="article_detail"),
     path("contact", contact__view, name="contact"),
     path("account/", include("account.urls")),
     path('admin/', admin.site.urls),
